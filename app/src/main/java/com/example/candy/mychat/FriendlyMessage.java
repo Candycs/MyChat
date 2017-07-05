98c7764d8209a7aa0d1ba078b5dd2117728ee5a4
Candy <chompunut.so.59@ubu.ac.th>
2017-07-05 15:26:31 +0700

    ...

---
+++ app/src/main/java/com/example/candy/mychat/FriendlyMessage.java
@@ -0,0 +1,34 @@
+package com.example.candy.mychat;
+
+/**
+ * Created by Candy on 5/7/2560.
+ */
+
+public class FriendlyMessage {
+    private String text;
+    private String username;
+
+    public FriendlyMessage(String text, String username) {
+        this.text = text;
+        this.username = username;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public FriendlyMessage() {
+    }
+}
